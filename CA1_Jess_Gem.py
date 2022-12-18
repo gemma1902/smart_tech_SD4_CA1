af3987c950415b2431e8e4285c4ed2f56f5953ab
jessicasavage1111 <D00227023@student.dkit.ie>
2022-12-18 20:59:15 +0000

    Trying to read Trining data

--- CA1_Jess_Gem.py
+++ CA1_Jess_Gem.py
@@ -39,7 +39,7 @@ def preprocessing(image):
 DATA = "F:\College\Year 4\Smart Tech\JessicaSavage_GemmaRegan_SmartTech_CA1/tiny-imagenet-200"
 VAL_ANNOTATIONS_PATH = "F:\College\Year 4\Smart Tech\JessicaSavage_GemmaRegan_SmartTech_CA1/tiny-imagenet-200/val/val_annotations.txt"
 VAL_PATH = "F:/College/Year 4/Smart Tech/JessicaSavage_GemmaRegan_SmartTech_CA1/tiny-imagenet-200/val/images"
-TRAIN_PATH = "F:/College/Year 4/Smart Tech/JessicaSavage_GemmaRegan_SmartTech_CA1/tiny-imagenet-200/train"
+TRAIN_PATH = "F:\\College\\Year 4\\Smart Tech\\JessicaSavage_GemmaRegan_SmartTech_CA1\\tiny-imagenet-200\\train"
 # TEST_PATH = "F:\College\Year 4\Smart Tech\JessicaSavage_GemmaRegan_SmartTech_CA1/tiny-imagenet-200/test"
 WNIDS_PATH = "F:\College\Year 4\Smart Tech\JessicaSavage_GemmaRegan_SmartTech_CA1/tiny-imagenet-200/wnids.txt"
 # WORDS_PATH = "F:\College\Year 4\Smart Tech\JessicaSavage_GemmaRegan_SmartTech_CA1/tiny-imagenet-200/words.txt"
@@ -68,9 +68,20 @@ for filename in os.listdir(VAL_PATH):
 #plt.show()
 
 
-# LOAD VALIDATION DATA
-#val_dataset = datasets.ImageFolder(VAL_PATH)
-#print("VALIDATION DATASET SIZE", len(val_dataset))
+# LOAD TRAINING DATA
+train_data = []
+for files in os.listdir(TRAIN_PATH):
+    j = os.path.join(TRAIN_PATH, files)
+    for file in os.listdir(j):
+        if(os.path.join(j, "images")):
+            k = os.path.join(j, file)
+            if os.path.isfile(k):
+                train_data.append(k)
+print(train_data[0])
+img = cv2.imread(train_data[4])
+plt.imshow(img)
+plt.show()
+
 
 #LOAD IN DATA FROM WNIDS TEXT FILE
 with open(WNIDS_PATH, "r") as file:
@@ -86,12 +97,6 @@ for i, wnid in enumerate(wnids): #Loop through and assign label
     pair[wnid] = i
 val_annotations = open(os.path.join(VAL, 'val_annotations.txt'), 'r')
 val_file = val_annotations.readlines()
-
-val_data = {}
-for line in val_file:
-    words = line.split('\t')
-    val_data[words[0]] = words[1]
-val_annotations.close()
 
 #print(label_data)
 
