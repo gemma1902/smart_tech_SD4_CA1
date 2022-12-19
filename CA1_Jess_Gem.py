3bd5f4023b0fb9d5142780221f34088b547bffde
jessicasavage1111 <D00227023@student.dkit.ie>
2022-12-19 20:34:41 +0000

    Check if images are 64x64 and the number of images equals number of labels

--- CA1_Jess_Gem.py
+++ CA1_Jess_Gem.py
@@ -38,22 +38,9 @@ def preprocessing(image):
     return image
 
 
-DATA = "F:\College\Year 4\Smart Tech\JessicaSavage_GemmaRegan_SmartTech_CA1/tiny-imagenet-200"
-VAL_ANNOTATIONS_PATH = "F:\College\Year 4\Smart Tech\JessicaSavage_GemmaRegan_SmartTech_CA1/tiny-imagenet-200/val/val_annotations.txt"
-VAL_PATH = "F:\\College\\Year 4\\Smart Tech\\JessicaSavage_GemmaRegan_SmartTech_CA1\\tiny-imagenet-200\\val\\images"
-TRAIN_PATH = "F:\\College\\Year 4\\Smart Tech\\JessicaSavage_GemmaRegan_SmartTech_CA1\\tiny-imagenet-200\\train"
-TEST_PATH = "F:\\College\\Year 4\\Smart Tech\\JessicaSavage_GemmaRegan_SmartTech_CA1\\tiny-imagenet-200\\test\\images"
-WNIDS_PATH = "F:\\College\\Year 4\\Smart Tech\\JessicaSavage_GemmaRegan_SmartTech_CA1\\tiny-imagenet-200\\wnids.txt"
-WORDS_PATH = "F:\College\Year 4\Smart Tech\JessicaSavage_GemmaRegan_SmartTech_CA1/tiny-imagenet-200/words.txt"
-num_classes=200
-
-
-
-
-
 # def leNet_model():
 #     model = Sequential()
-#     model.add(Conv2D(60, (5, 5), input_shape=(32, 32, 1), activation='relu'))  # 30 = number of filters 5x5 = kernal 32x32 === 28x28 output
+#     model.add(Conv2D(60, (5, 5), input_shape=(64, 64, 1), activation='relu'))  # 30 = number of filters 5x5 = kernal 32x32 === 28x28 output
 #     model.add(MaxPooling2D(pool_size=(2, 2))) #14x14 = output
 #     model.add(Conv2D(30, (3, 3), activation='relu')) #12x12 = output
 #     model.add(MaxPooling2D(pool_size=(2, 2))) #6x6 = output
@@ -65,155 +52,15 @@ num_classes=200
 #     model.compile(Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy']) #adam optimizer
 #     return model
 
-# # LOAD WNIDS INTO ARRAY
-# WNIDS = open(os.path.join(DATA, 'wnids.txt'), 'r')
-# WNIDS_file = WNIDS.readlines()
-# WNIDS_data = []
-# for line in WNIDS_file:
-#     words = line.split('\t')
-#     WNIDS_data.append(words)
-# WNIDS.close()
-#
-#
-#
-# # img = Image.open(k)
-# #             X_train_numpy_data = np.asarray(img)
-# #             # print(X_train_numpy_data)
-# #             # X_train.append(X_train_numpy_data)
-# #             # print(type(X_train))
-# #             X_train = np.asarray([X_train_numpy_data])
-# #             # print(X_train.shape)
-# #             # print(" x train ====== ", type(X_train))
-#
-# # LOAD VALIDATION DATA
-# X_val = []
-# for filename in os.listdir(VAL_PATH):
-#     f = os.path.join(VAL_PATH, filename)
-#     if os.path.isfile(f):
-#         img = Image.open(os.path.join(VAL_PATH,filename)).convert('RGB')
-#         X_val_numpy_data = np.asarray(img)
-#         X_val = np.asarray([X_val_numpy_data])
-#
-# print(X_val)
-# print(X_val.shape)
-#
-# # LOAD TEST DATA
-# X_test = []
-# for file in os.listdir(TEST_PATH):
-#     h = os.path.join(TEST_PATH, file)
-#     if os.path.isfile(h):
-#         img = Image.open(h)
-#         X_test_numpy_data = asarray(img)
-#         X_test.append(X_test_numpy_data)
-#
-# # print(test_data[0])
-# # img = cv2.imread(test_data[4])
-# # plt.imshow(img)
-# # plt.show()
-#
-#
-# # LOAD TRAINING DATA
-# X_train = []
-# y_train = []
-# for file in os.listdir(TRAIN_PATH):
-#     y_train.append(file)
-#     # print(y_train)
-#     h = os.path.join(TRAIN_PATH,file)
-#     j = os.path.join(h,"images")
-#     for files in os.listdir(j):
-#         k = os.path.join(j, files)
-#         if os.path.isfile(k):
-#             img = Image.open(k)
-#             X_train_numpy_data = np.asarray(img)
-#             # print(X_train_numpy_data)
-#             # X_train.append(X_train_numpy_data)
-#             # print(type(X_train))
-#             X_train = np.asarray([X_train_numpy_data])
-#             # print(X_train.shape)
-#             # print(" x train ====== ", type(X_train))
-#
-# print(X_train)
-#
-# # print(X_train[0])
-# # img = cv2.imread(X_train[4])
-# # plt.imshow(img)
-# # plt.show()
-#
-#
-# # LOAD IN DATA FROM WNIDS TEXT FILE
-# with open(WNIDS_PATH, "r") as file:
-#     wnids = file.read()
-# # Directory to datasets
-# TRAIN = os.path.join(DATA, 'train')
-# VAL = os.path.join(DATA, 'val')
-# TEST = os.path.join(DATA, 'test')
-#
-# # PAIR EACH WNIDS NUMBER TO AN INTERGER I.E. {n08966554 : 1, n0897856 : 2} etc.
-# pair = {}
-# for i, wnid in enumerate(wnids):  # Loop through and assign label
-#     pair[wnid] = i
-#
-# y_val = []
-# with open(VAL_ANNOTATIONS_PATH, "r") as file:
-#     val_y = file.readlines()
-#     for line in val_y:
-#         words = line.split()
-#         val_label = words[1]
-#         y_val.append(val_label)
 
-# print(label_data)
-
-# Use words.txt to get names for each class  #not working fully
-# with open(WORDS_PATH, 'r') as f:
-#     wnid_to_words = dict(line.split('\t') for line in f)
-#     for wnid, words in wnid_to_words.items():
-#       wnid_to_words[wnid] = [w.strip() for w in words.split(',')]
-# class_names = [wnid_to_words[wnid] for wnid in wnids]
-
-
-
-# plt.figure(figsize=(12, 4))
-# plt.bar(range(0, num_classes), num_of_samples)
-# plt.title("Distribution of training dataset")
-# plt.xlabel("class number")
-# plt.ylabel("NUmber of Imgs")
-# plt.show()
-
-# assert(X_train.shape[0] == y_train.shape[0]), "the number of training images is not equal to the number of training labels"
-# assert(X_val.shape[0] == y_val.shape[0]), "the number of validation images is not equal to the number of validation labels"
-# # assert(X_test.shape[0] == y_test.shape[0]), "the number of test images is not equal to the number of test labels"
-# assert(X_train.shape[1:] == (64, 64, 3)), "the dimensions of the training images are not 32x32x3"
-# assert(X_val.shape[1:] == (64, 64, 3)), "the dimensions of the validation images are not 32x32x3"
-# assert(X_test.shape[1:] == (64, 64, 3)), "the dimensions of the test images are not 32x32x3"
-
-# DATA_PREPROCESSING
-# print(X_train.shape)
-# print(X_val.shape)
-# print(X_test.shape)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+DATA = "F:\College\Year 4\Smart Tech\JessicaSavage_GemmaRegan_SmartTech_CA1/tiny-imagenet-200"
+VAL_ANNOTATIONS_PATH = "F:\College\Year 4\Smart Tech\JessicaSavage_GemmaRegan_SmartTech_CA1/tiny-imagenet-200/val/val_annotations.txt"
+VAL_PATH = "F:\\College\\Year 4\\Smart Tech\\JessicaSavage_GemmaRegan_SmartTech_CA1\\tiny-imagenet-200\\val\\images"
+TRAIN_PATH = "F:\\College\\Year 4\\Smart Tech\\JessicaSavage_GemmaRegan_SmartTech_CA1\\tiny-imagenet-200\\train"
+TEST_PATH = "F:\\College\\Year 4\\Smart Tech\\JessicaSavage_GemmaRegan_SmartTech_CA1\\tiny-imagenet-200\\test\\images"
+WNIDS_PATH = "F:\\College\\Year 4\\Smart Tech\\JessicaSavage_GemmaRegan_SmartTech_CA1\\tiny-imagenet-200\\wnids.txt"
+WORDS_PATH = "F:\College\Year 4\Smart Tech\JessicaSavage_GemmaRegan_SmartTech_CA1/tiny-imagenet-200/words.txt"
+num_classes=200
 
 
 
@@ -312,22 +159,28 @@ for file in os.listdir(TEST_PATH):
 X_test = np.array(X_test_data)
 #print("X_TEST : ", X_test)
 
-
+# DATA_PREPROCESSING
 # Print number of Images
 print("X_TRAIN SHAPE : ", X_train.shape)
 print("X_VAL SHAPE : ", X_val.shape)
 print("X_TEST SHAPE : ", X_test.shape)
 
-# assert(X_train.shape[0] == y_train.shape[0]), "the number of training images is not equal to the number of training labels"
-# assert(X_val.shape[0] == y_val.shape[0]), "the number of validation images is not equal to the number of validation labels"
-# assert(X_test.shape[0] == y_test.shape[0]), "the number of test images is not equal to the number of test labels"
-# assert(X_train.shape[1:] == (64, 64, 3)), "the dimensions of the training images are not 64x64x3"
-# assert(X_val.shape[1:] == (64, 64, 3)), "the dimensions of the validation images are not 64x64x3"
-# assert(X_test.shape[1:] == (64, 64, 3)), "the dimensions of the test images are not 64x64x3"
+# Is Number of labels == number of images
+assert(X_train.shape[0] == y_train.shape[0]), "the number of training images is not equal to the number of training labels"
+assert(X_val.shape[0] == y_val.shape[0]), "the number of validation images is not equal to the number of validation labels"
+#assert(X_test.shape[0] == y_test.shape[0]), "the number of test images is not equal to the number of test labels"
 
-# X_train = np.array(list(map(preprocessing, X_train))) #avoid loops and use maps
-# X_val = np.array(list(map(preprocessing, X_val)))
-# X_test = np.array(list(map(preprocessing, X_test)))
+# Check all images are 64 x 64
+assert(X_train.shape[1:] == (64, 64, 3)), "the dimensions of the training images are not 64x64x3"
+assert(X_val.shape[1:] == (64, 64, 3)), "the dimensions of the validation images are not 64x64x3"
+assert(X_test.shape[1:] == (64, 64, 3)), "the dimensions of the test images are not 64x64x3"
+
+
+# Apply preprocessing method to every image
+# Map means you don't have to use a forLoop
+X_train = np.array(list(map(preprocessing, X_train))) #avoid loops and use maps
+X_val = np.array(list(map(preprocessing, X_val)))
+X_test = np.array(list(map(preprocessing, X_test)))
 
 #one hot encode labels
 # y_train = to_categorical(y_train, num_classes)
