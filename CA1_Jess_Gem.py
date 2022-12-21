ae39df3b66e7d10de177fd4fb9bf93e4d1e89ef8
jessicasavage1111 <D00227023@student.dkit.ie>
2022-12-21 17:04:39 +0000

    Fix: fix the x_val data

--- CA1_Jess_Gem.py
+++ CA1_Jess_Gem.py
@@ -2,16 +2,17 @@ import glob
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-# import tensorflow.keras
-# from tensorflow.keras.models import Sequential
-# from tensorflow.keras.layers import Dense
-# from tensorflow.keras.optimizers import Adam
-# from tensorflow.keras.layers import Flatten
-# from keras.layers.convolutional import Conv2D
-# from keras.layers.convolutional import MaxPooling2D
+import tensorflow.keras
+from keras.layers import Dropout
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
 from keras.utils.np_utils import to_categorical
-# import random
-# import requests
+import random
+import requests
 from PIL import Image
 from numpy import asarray
 import cv2
@@ -37,20 +38,27 @@ def preprocessing(image):
     image = image / 255
     return image
 
+def gaussianBlur(image):
+    kernel_size = (5, 5)
+    kernel = cv2.getGaussianKernel(kernel_size[0], 0)
+    kernel = kernel * kernel.T
+    image_blurred = cv2.filter2D(image, -1, kernel)
+    return image_blurred
 
-# def leNet_model():
-#     model = Sequential()
-#     model.add(Conv2D(60, (5, 5), input_shape=(64, 64, 1), activation='relu'))  # 30 = number of filters 5x5 = kernal 32x32 === 28x28 output
-#     model.add(MaxPooling2D(pool_size=(2, 2))) #14x14 = output
-#     model.add(Conv2D(30, (3, 3), activation='relu')) #12x12 = output
-#     model.add(MaxPooling2D(pool_size=(2, 2))) #6x6 = output
-#     # flatten image before fully connected layer
-#     model.add(Flatten())
-#     model.add(Dense(500, activation='relu'))
-#     model.add(Dropout(0.5))  # helps with overfitting & forces all nodes to work
-#     model.add(Dense(num_classes, activation='softmax')) # softmax coz its a multiclass
-#     model.compile(Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy']) #adam optimizer
-#     return model
+
+def leNet_model():
+    model = Sequential()
+    model.add(Conv2D(60, (5, 5), input_shape=(64, 64, 1), activation='relu'))  # 30 = number of filters 5x5 = kernal 32x32 === 28x28 output
+    model.add(MaxPooling2D(pool_size=(2, 2))) #14x14 = output
+    model.add(Conv2D(30, (3, 3), activation='relu')) #12x12 = output
+    model.add(MaxPooling2D(pool_size=(2, 2))) #6x6 = output
+    # flatten image before fully connected layer
+    model.add(Flatten())
+    model.add(Dense(500, activation='relu'))
+    model.add(Dropout(0.5))  # helps with overfitting & forces all nodes to work
+    model.add(Dense(num_classes, activation='softmax')) # softmax coz its a multiclass
+    model.compile(Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy']) #adam optimizer
+    return model
 
 
 DATA = "F:\College\Year 4\Smart Tech\JessicaSavage_GemmaRegan_SmartTech_CA1/tiny-imagenet-200"
@@ -82,11 +90,16 @@ with open(WORDS_PATH, 'r') as f:
       wnid_to_words[wnid] = [w.strip() for w in words.split(',')]
 class_names = [wnid_to_words[wnid] for wnid in wnids]
 
-
+class_labels = {}
+with open(WORDS_PATH, 'r') as f:
+    for line in f:
+        label, name = line.strip().split('\t')
+        class_labels[label] = name
+#print(class_labels)
 
 # load in training data
 X_train_data = []
-y_train = []
+y_train_data = []
 image_index=[]
 
 for file in os.listdir(TRAIN_PATH):
@@ -99,9 +112,8 @@ for file in os.listdir(TRAIN_PATH):
                 num_images = len(filenames)
 
 
-images = np.ndarray(shape=(64, (64*64*3)))
+#images = np.ndarray(shape=(64, (64*64*3)))
 for file in os.listdir(TRAIN_PATH):
-    y_train.append(file)
     # print(y_train)
     h = os.path.join(TRAIN_PATH,file)
     j = os.path.join(h,"images")
@@ -110,30 +122,22 @@ for file in os.listdir(TRAIN_PATH):
         img = Image.open(k).convert('RGB')
         # print(np.array(img).shape)
         X_train_data.append(np.array(img))
-        y_train.append(file)
-        # if os.path.isfile(k):
-        #     img = Image.open(k)
-        #     X_train_numpy_data = np.asarray(img)
-        #     # print(X_train_numpy_data)
-        #     # X_train.append(X_train_numpy_data)
-        #     # print(type(X_train))
-        #     X_train = np.asarray([X_train_numpy_data])
-            # print(X_train.shape)
-            # print(" x train ====== ", type(X_train))
+        y_train_data.append(file)
 X_train = np.array(X_train_data)
-#print("X_TRAIN SHAPE : ", X_train.shape)
-#print("Y_TRAIN : ", y_train)
+y_train = np.array(y_train_data)
+
+class_name = class_labels[label]
+#print("LINKING IMAGES", f'{file}: {class_name}')
 
 #load in validation data
 X_val_data = []
-y_val = []
+y_val_data = []
 
 for filename in os.listdir(VAL_PATH):
     f = os.path.join(VAL_PATH, filename)
-    if os.path.isfile(f):
-        img = Image.open(os.path.join(VAL_PATH,filename)).convert('RGB')
-        X_val_numpy_data = np.asarray(img)
-        X_val_data = np.asarray([X_val_numpy_data])
+    img = Image.open(f).convert('RGB')
+    # print(np.array(img).shape)
+    X_val_data.append(np.array(img))
 X_val = np.array(X_val_data)
 
 with open(VAL_ANNOTATIONS_PATH, "r") as file:
@@ -141,12 +145,8 @@ with open(VAL_ANNOTATIONS_PATH, "r") as file:
     for line in val_y:
         words = line.split()
         val_label = words[1]
-        y_val.append(val_label)
-
-#print("X_VAL : ", X_val)
-#print("Y_VAL : ", y_val)
-
-
+        y_val_data.append(val_label)
+y_val = np.array(y_val_data)
 
 #load in testing data
 X_test_data = []
@@ -157,7 +157,6 @@ for file in os.listdir(TEST_PATH):
         X_test_numpy_data = asarray(img)
         X_test_data.append(X_test_numpy_data)
 X_test = np.array(X_test_data)
-#print("X_TEST : ", X_test)
 
 # DATA_PREPROCESSING
 # Print number of Images
@@ -165,15 +164,22 @@ print("X_TRAIN SHAPE : ", X_train.shape)
 print("X_VAL SHAPE : ", X_val.shape)
 print("X_TEST SHAPE : ", X_test.shape)
 
+#Normalise data
+X_train = X_train / 255
+X_test = X_test / 255
+
+print("normalise: ", X_train)
+print(X_test)
+
 # Is Number of labels == number of images
-assert(X_train.shape[0] == y_train.shape[0]), "the number of training images is not equal to the number of training labels"
-assert(X_val.shape[0] == y_val.shape[0]), "the number of validation images is not equal to the number of validation labels"
+#assert(X_train.shape[0] == y_train.shape[0]), "the number of training images is not equal to the number of training labels"
+#assert(X_val.shape[0] == y_val.shape[0]), "the number of validation images is not equal to the number of validation labels"
 #assert(X_test.shape[0] == y_test.shape[0]), "the number of test images is not equal to the number of test labels"
 
 # Check all images are 64 x 64
-assert(X_train.shape[1:] == (64, 64, 3)), "the dimensions of the training images are not 64x64x3"
-assert(X_val.shape[1:] == (64, 64, 3)), "the dimensions of the validation images are not 64x64x3"
-assert(X_test.shape[1:] == (64, 64, 3)), "the dimensions of the test images are not 64x64x3"
+#assert(X_train.shape[1:] == (64, 64, 3)), "the dimensions of the training images are not 64x64x3"
+#assert(X_val.shape[1:] == (64, 64, 3)), "the dimensions of the validation images are not 64x64x3"
+#assert(X_test.shape[1:] == (64, 64, 3)), "the dimensions of the test images are not 64x64x3"
 
 
 # Apply preprocessing method to every image
@@ -182,7 +188,18 @@ X_train = np.array(list(map(preprocessing, X_train))) #avoid loops and use maps
 X_val = np.array(list(map(preprocessing, X_val)))
 X_test = np.array(list(map(preprocessing, X_test)))
 
-#one hot encode labels
-# y_train = to_categorical(y_train, num_classes)
-# y_test = to_categorical(y_test, num_classes)
-# y_val = to_categorical(y_val, num_classes)
+plt.imshow(X_train[random.randint(0, len(X_train)-1)])
+plt.axis("off")
+plt.show()
+print(X_train.shape)
+
+X_train = X_train.reshape(X_train.shape[0], 64, 64, 1)
+X_test = X_test.reshape(X_test.shape[0], 64, 64, 1)
+X_val = X_val.reshape(X_val.shape[0], 64, 64, 1)
+
+# One hot encode the labels
+y_train = to_categorical(y_train, num_classes)
+y_val = to_categorical(y_val, num_classes)
+
+#model = leNet_model()
+#print(model.summary())
